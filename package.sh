ea2a0db1947c0d55ea29af280484a19a8e243f2b
mins1710 <tranhuy.dev17@gmail.com>
2024-03-31 13:47:32 +0700

    zz

--- package.sh
+++ package.sh
@@ -1,6 +1,7 @@
 mv ./com.mins.appclonerd_* ./com.mins.appclonerd.deb
 mv ./net.angelxwind.appsyncunified_* ./net.angelxwind.appsyncunified.deb
 mv ./NEPacketTunnelVPNDemo ./NEPacketTunnelVPNDemo.ipa
+git lfs track "*.ipa"
 git add .
 git commit -m "zz"
 git push origin main
