696c52e751384b4cf9fd53acd883807bf5d8e752
mins1710 <tranhuy.dev17@gmail.com>
2024-03-31 14:20:35 +0700

    zz

--- package.sh
+++ package.sh
@@ -1,3 +1,4 @@
+mv ./com.linusyang.appinst_* ./com.linusyang.appinst.deb
 mv ./com.mins.appclonerd_* ./com.mins.appclonerd.deb
 mv ./net.angelxwind.appsyncunified_* ./net.angelxwind.appsyncunified.deb
 mv ./NEPacketTunnelVPNDemo ./NEPacketTunnelVPNDemo.ipa
