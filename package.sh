c833aa498ffc36f09a81ff44ca00fe61f59fdb57
mins1710 <tranhuy.dev17@gmail.com>
2024-03-31 13:45:20 +0700

    zz

---
+++ package.sh
@@ -0,0 +1,6 @@
+mv ./com.mins.appclonerd_* ./com.mins.appclonerd.deb
+mv ./net.angelxwind.appsyncunified_* ./net.angelxwind.appsyncunified.deb
+mv ./NEPacketTunnelVPNDemo ./NEPacketTunnelVPNDemo.ipa
+git add .
+git commit -m "zz"
+git push origin main
