3436d11191e4bcac6aad11a1e9b3341f8e5d237a
mins1710 <tranhuy.dev17@gmail.com>
2024-03-31 14:31:04 +0700

    zz

--- package.sh
+++ package.sh
@@ -1,6 +1,6 @@
-mv ./com.linusyang.appinst_* ./com.linusyang.appinst.deb
-mv ./com.mins.appclonerd_* ./com.mins.appclonerd.deb
-mv ./net.angelxwind.appsyncunified_* ./net.angelxwind.appsyncunified.deb
+# mv ./com.linusyang.appinst_* ./com.linusyang.appinst.deb
+# mv ./com.mins.appclonerd_* ./com.mins.appclonerd.deb
+# mv ./net.angelxwind.appsyncunified_* ./net.angelxwind.appsyncunified.deb
 mv ./NEPacketTunnelVPNDemo ./NEPacketTunnelVPNDemo.ipa
 git lfs track "*.ipa"
 git add .
